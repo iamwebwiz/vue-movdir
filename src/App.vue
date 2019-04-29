b5a608d73fed549ae6557d4321553ecb77792605
Ezekiel <iamwebwiz@gmail.com>
2019-04-29 18:04:45 +0100

    Update App component

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,27 @@
     <section class="hero">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title has-text-success">
-            <span class="icon">
-              <i class="fas fa-video"></i>
-            </span>
-            &nbsp;
-            Vue Movies CRUD
-          </h1>
-          <h2 class="subtitle">Manage Movies in one place</h2>
+          <div class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <h1 class="title has-text-success">
+                  <span class="icon">
+                    <i class="fas fa-video"></i>
+                  </span>
+                  &nbsp;
+                  Vue Movies CRUD
+                </h1>
+              </div>
+              <div class="level-item">
+                <h2 class="subtitle">Manage Movies in one place</h2>
+              </div>
+            </div>
+            <div class="level-right">
+              <div class="level-item">
+                <button class="button is-info">Add Movie</button>
+              </div>
+            </div>
+          </div>
           <hr>
           <Home/>
         </div>
