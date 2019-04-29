ee16ff4783c0f03c2061d234b3f5b13e1d0ec4b2
Ezekiel <iamwebwiz@gmail.com>
2019-04-29 11:46:51 +0100

    Add EditMovie component

---
+++ src/views/EditMovie.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="modal" id="editMovieModal">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Edit Movie Info</p>
+          <button class="delete" aria-label="close" @click="closeModal"></button>
+        </header>
+        <section class="modal-card-body">
+          <!-- Content ... -->
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Save changes</button>
+          <button class="button" @click="closeModal">Cancel</button>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "EditMovie",
+    props: ["movie"],
+    methods: {
+      closeModal() {
+        document.querySelector("#editMovieModal").classList.remove("is-active");
+        document.querySelector("html").classList.remove("is-clipped");
+      }
+    }
+  };
+</script>
